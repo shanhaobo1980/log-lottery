356354c57e6a4a5f98d8eea7bb334a7b3b1ddb6e
shanhaobo1980 <shanhaobo1980@users.noreply.github.com>
2026-01-20 08:08:22 +0000

    deploy: 477145b284b2122d989aba53f792e867742ca833

---
+++ js/chunk-DlN4IvmF.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./chunk-6arlBFx2.js";import{K as o,o as s,J as t}from"./chunk-B1GhtS-6.js";const n=r({},[["render",function(r,n){const e=t("router-view");return s(),o(e)}]]);export{n as default};
